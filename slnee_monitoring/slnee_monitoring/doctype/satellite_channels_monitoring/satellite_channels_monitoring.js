4cb71eeb828525ac472e00750fca84f8259fb4b3
mghoneem10 <mghoneem10@gmail.com>
2021-08-18 23:16:19 +0000

    x

---
+++ slnee_monitoring/slnee_monitoring/doctype/satellite_channels_monitoring/satellite_channels_monitoring.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, erpcloud.systems and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Satellite Channels Monitoring', {
+	// refresh: function(frm) {
+
+	// }
+});
